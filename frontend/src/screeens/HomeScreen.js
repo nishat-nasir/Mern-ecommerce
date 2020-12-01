3ff5b8733ba64165805ceea07a0bbd08042a1245
Nishat Nasir <nishatnasir00@gmail.com>
2020-12-01 13:08:15 +0900

    added Error and loading ui

--- frontend/src/screeens/HomeScreen.js
+++ frontend/src/screeens/HomeScreen.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import {listProducts} from '../actions/productActions'
 
 
@@ -20,7 +21,7 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
-            { loading ? (<h2>Loading...</h2>) : error ? (<h3>{error}</h3>) : (
+            { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Row>
                     {products.map( ( product ) => (
 
